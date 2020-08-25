1308755945abcd0a0fd25240f28bd04520b74204
ulf gjerdingen <ulf@ulf.local>
2020-08-25 23:20:43 +0200

    usage added to README and simple cli added

--- README.md
+++ README.md
@@ -3,15 +3,35 @@ child_allowance
 system for keeping track of my 3 kid's allowances & expenses.
 
 
+## usage
+
+create Tony and give him 40 bux to start his life:
+```bash
+$ python child_allowance.py --create Tony 40
+
+# csv => Tony;1;2020-04-06 19:50:04;40;40;creation of record
+```
+
+give Tony some money for a good job done:
+```bash
+$ python child_allowance.py Tony 5 "weekend child labor"
+
+# csv => Tony;1;2020-04-20 16:20:56;5;45;weekend child labor
+```
+
+Tony wants to spend some money on candy:
+```bash
+$ python child_allowance.py Tony -15 "expensive candy"
+
+# csv => Tony;1;2020-08-25 10:20:03;-15;30;expensive candy
+```
+
 ### TODO
 
 soon
 
-* set up up sys.args for adding transactions
-* child creation (not the fun kind...)
-    - perhaps with option -c, --create ?
+* better command-line interface for adding transactions?
 
 later
 
-* better command-line interface for adding transactions
 * automatically add amount for each period child receives allowance
--- child_allowance.py
+++ child_allowance.py
@@ -3,11 +3,14 @@ import csv
 import argparse
 
 ap = argparse.ArgumentParser()
-ap.add_argument('--create', nargs='?')
+ap.add_argument('--create', action='store_true')
 ap.add_argument('childname')
 ap.add_argument('amount', type=int)
 ap.add_argument('description', nargs='?')
 args = ap.parse_args()
+
+transaction_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+header = ['childname', 'transaction_id', 'transaction_date', 'transaction_amount', 'current_amount', 'description']
 
 
 class Child:
@@ -19,6 +22,7 @@ class Child:
         self.transaction_amount = transaction_amount
 
 
+
     def get_current_data(self):
         """Reads latest entry (last line) in database for instance."""
         with open(f'{self.childname}_sample.csv', 'r') as csv_file:
@@ -26,61 +30,53 @@ class Child:
             return list(reader)[-1]
 
 
+    def create_child(self):
+        """Setup a file for a new child to track."""
+        child_data = [
+            self.childname,
+            1,  # transaction id
+            transaction_date,
+            self.transaction_amount,
+            self.transaction_amount,  # no old amount existed before
+            'creation of record'
+        ]
+        with open(f'{self.childname}_sample.csv', mode='w+') as child_file:
+            child_writer = csv.writer(child_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            child_writer.writerow(header)
+            child_writer.writerow(child_data)
+
+
     def store_transaction(self, data):
         """Stores transaction in database."""
+        current_amount = int(data[4])
+        transaction_id = int(data[1]) + 1
+        transaction_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        new_amount = current_amount + self.transaction_amount
+        # print(f"{self.childname}  {transaction_id}  {transaction_date}  {self.transaction_amount}  {new_amount}  {self.description}")
+        print(header)
+        child_data = [
+            self.childname,
+            transaction_id,
+            transaction_date,
+            self.transaction_amount,
+            new_amount,
+            self.description
+        ]
 
-        # if create child
-        if args.create:
-
-            # create new file
-            header = [self.childname, transaction_id, transaction_date, self.transaction_amount, new_amount, self.description]
-            with open(f'{self.childname}_sample.csv', mode='w+') as child_file:
-                child_writer = csv.writer(child_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                child_writer.writerow(header)
-
-            print('creation time!')
-            # child_data = [
-            #     self.childname,
-            #     transaction_id,
-            #     transaction_date,
-            #     self.transaction_amount,
-            #     self.transaction_amount, # no old amount existed before
-            #     'creation'
-            # ]
-
-            with open(f'{self.childname}_sample.csv', mode='a+') as child_file:
-                child_writer = csv.writer(child_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                child_writer.writerow(child_data)
-
-        # if add transaction for existing child
-        else:
-            # current_amount = int(data[4])
-            # transaction_id = int(data[1]) + 1
-            # transaction_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            # new_amount = current_amount + self.transaction_amount
-
-            # print(f"{self.childname}  {transaction_id}  {transaction_date}  {self.transaction_amount}  {new_amount}  {self.description}")
-
-            # child_data = [
-            #     self.childname,
-            #     transaction_id,
-            #     transaction_date,
-            #     self.transaction_amount,
-            #     new_amount,
-            #     self.description
-            # ]
-
-            # with open(f'{self.childname}_sample.csv', mode='a+') as child_file:
-            #     child_writer = csv.writer(child_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            #     child_writer.writerow(child_data)
-
-
-
-
+        with open(f'{self.childname}_sample.csv', mode='a+') as child_file:
+            child_writer = csv.writer(child_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            child_writer.writerow(child_data)
 
 
 if __name__ == '__main__':
-    # Child(name, description, amount)
-    child1 = Child('child1', 'candy', -5)
-    current_data = child1.get_current_data()
-    child1.store_transaction(current_data)
+    # # Child(name, description, amount)
+    # child1 = Child('child1', 'candy', -5)
+    # current_data = child1.get_current_data()
+    # child1.store_transaction(current_data)
+
+    child = Child(args.childname, args.description, args.amount)
+    if args.create:
+        child.create_child()
+    else:
+        current_data = child.get_current_data()
+        child.store_transaction(current_data)
