f98bba5beacd58f5b9d85bacc041053d037cca13
ulf gjerdingen <ulf@ulf.local>
2020-08-25 16:49:41 +0200

    improved logging and removed temp variables

--- child_allowance.py
+++ child_allowance.py
@@ -7,22 +7,21 @@ class Child:
 
 
     def __init__(self, childname, description, transaction_amount=2):
+        self.childname = childname
         self.description = description
         self.transaction_amount = transaction_amount
-        # self.num_of_transactions = 0
+
 
     def add_money(self):
         """"Add amount for period."""
         self.current_amount = self.current_amount + self.raise_amount
         self.num_of_transactions += 1
-        self.num_of_add += 1
 
 
     def use_money(self, used_amount):
         """Subtracts from current_amount when child uses money."""
         self.current_amount + (self.transaction_amount)  # addition used; cus x + (-y) is correct
         self.num_of_transactions += 1
-        self.num_of_use += 1
 
 
     def get_current_data(self):
@@ -34,18 +33,17 @@ class Child:
 
     def store_transaction(self, data):
         """Stores transaction in database."""
-        childname = data[0]
         current_amount = int(data[4])
         transaction_id = int(data[1]) + 1
         transaction_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         new_amount = current_amount + self.transaction_amount
 
-        print(f"{childname}  {transaction_id}  {transaction_date}  {self.transaction_amount}  {new_amount}  {self.description}")
+        print(f"{self.childname}  {transaction_id}  {transaction_date}  {self.transaction_amount}  {new_amount}  {self.description}")
 
         with open('sample_csv_michael.csv', mode='a') as child_file:
             child_writer = csv.writer(child_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             child_writer.writerow([
-                childname,  # childname
+                self.childname,  # childname
                 transaction_id,  # transaction_id
                 transaction_date,  # transaction_date
                 self.transaction_amount,  # transaction_amount
